c5fe59bce0c92f42e6a18fcaff6888911a5b4ec5
Eugene Cheah (@picocreator) <eugeneqin@gmail.com>
2023-05-08 22:38:52 +0800

    WIP sub repo integration

---
+++ 2-TRN/30-Law/constitution-multi-lang/clone-repo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Change to the directory where this file is located
+cd "$(dirname "$0")"
+
+# Setup and clone the repo locally
+git clone https://huggingface.co/datasets/picocreator/constitution-multi-lang ./constitution-multi-lang.git/
